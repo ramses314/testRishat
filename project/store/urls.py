414e87505c83cb3fd1bb973c7cbec2544aac86c9
ramses314 <forwork31415@gmail.com>
2022-12-02 19:25:42 +0600

    test

--- project/store/urls.py
+++ project/store/urls.py
@@ -1,18 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
 from .views import *
 
 app_name = 'store'
 urlpatterns = [
-    path('', index, name='home'),
-
-    # Stripe urls
-    path('create-checkout-session/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('', home, name='home'),
 
     # API request
-    path('buy/<int:id>/', BuyView.as_view(), name='buy_item'),
-    path('item/<int:id>/', ItemView.as_view(), name='get_item'),
+    path('buy/<int:id>/', ProductBuyView.as_view(), name='buy_item'),
+    path('item/<int:id>/', ProductView.as_view(), name='get_item'),
 
     # After Pay
     path('success/', SuccessView.as_view(), name='success_payment'),
--- project/store/views.py
+++ project/store/views.py
@@ -14,66 +14,43 @@ import stripe
 
 load_dotenv()
 
-API_KEY = os.getenv('STRIPE_PRIVATE_KEY')
-PUB_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+API_SECRET_KEY = os.getenv('STRIPE_PRIVATE_KEY')
+API_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 
-class CreateCheckoutSessionView(View):
-    def post(self, request, *args, **kwargs):
-        YOUR_DOMAIN = 'http://127.0.0.1:8000'
-        checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': '{{PRICE_ID}}',
-                    'quantity': 1,
-                },
-            ],
-            mode='payment',
-            success_url=YOUR_DOMAIN + '/success.html',
-            cancel_url=YOUR_DOMAIN + '/cancel.html',
-        )
-        return JsonResponse(
-            {
-                "id" : checkout_session.id
-            }
-        )
 
-def index(request):
-
+def home(request):
     products = Item.objects.all()
-
     context = {
-        'products' : products,
-        'api_key' : API_KEY,
-        'public_key' : PUB_KEY
+        'products': products,
+        'api_key': API_SECRET_KEY,
+        'public_key': API_PUBLIC_KEY
     }
     return render(request, 'store/index.html', context=context)
 
 
-
-
-class ItemView(TemplateView):
+class ProductView(TemplateView):
     template_name = 'store/detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['item'] = get_object_or_404(Item, id=self.kwargs['id'])
-        context['api_key'] = API_KEY
-        context['public_key'] = PUB_KEY
+        context['api_key'] = API_SECRET_KEY
+        context['public_key'] = API_PUBLIC_KEY
         return context
 
-class BuyView(RetrieveAPIView):
+
+class ProductBuyView(RetrieveAPIView):
     def retrieve(self, request, *args, **kwargs):
-        item = get_object_or_404(Item, id=self.kwargs['id'])
+        product = get_object_or_404(Item, id=self.kwargs['id'])
         checkout_session = stripe.checkout.Session.create(
-            api_key=API_KEY,
+            api_key=API_SECRET_KEY,
             line_items=[
                 {
                     'price_data': {
-                        'unit_amount': item.price * 100,
-                        'currency': item.currency,
+                        'unit_amount': product.price * 100,
+                        'currency': product.currency,
                         'product_data': {
-                            'name': item.name
+                            'name': product.name
                         }
                     },
                     'quantity': 1,
@@ -81,14 +58,15 @@ class BuyView(RetrieveAPIView):
             ],
             payment_method_types=['card'],
             mode='payment',
-            success_url='http://localhost/success/',
-            cancel_url='http://localhost/cancel/'
+            success_url='http://127.0.0.1:8000/success/',
+            cancel_url='http://127.0.0.1:8000/cancel/'
         )
         return Response(data={'sessionId': checkout_session['id'], })
 
+
 class SuccessView(TemplateView):
     template_name = 'store/success.html'
+
+
 class CancelView(TemplateView):
     template_name = 'store/cancel.html'
-
-
